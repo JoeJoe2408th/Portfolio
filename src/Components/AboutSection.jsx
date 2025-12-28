7054df8bb0446a3b4f098c40d4975569f965df9b
JoeJoe2408th <vuleducduy@gmail.com>
2025-12-28 20:46:50 +0700

    Fix Code

--- src/Components/AboutSection.jsx
+++ src/Components/AboutSection.jsx
@@ -30,7 +30,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://www.topcv.vn/xem-cv/AltdVQVdDQEAV1lZUABUDgJSCAZYAloEWl9UAQ085f"
+                href="https://www.topcv.vn/xem-cv/AAcEVw5VBAIEV1JQUVUBBA9YAQVUUgAAAVdXDA53bb"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -24,7 +24,7 @@ export const Navbar = () => {
   }, []);
   return (
     <nav
-      className={cn(
+      className={cn( 
         "fixed w-full z-40 transition-all duration-300",
         isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
       )}
