0dd8d586131bfec4741078c99a9c85047ead24ae
JoeJoe2408th <vuleducduy@gmail.com>
2025-12-31 13:19:13 +0700

    Fix Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -17,10 +17,10 @@ export const Navbar = () => {
   useEffect(() => {
     const handleScroll = () => {
       setIsScrolled(window.scrollY > 10);
-    };
+    }
 
     window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);  
   }, []);
   return (
     <nav
